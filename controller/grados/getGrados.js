7498b52787c8f3c45aba1dcfb221056fd925a27b
gitjulio <julio.ezequiel@gmail.com>
2019-06-24 22:25:11 -0600

    grado get

--- controller/grados/getGrados.js
+++ controller/grados/getGrados.js
@@ -6,7 +6,7 @@ exports.getGrados = async function(req, res, next) {
 
 
   let grados;
-  grados = await pg.func('public.ft_view_get_grados_instituto',[req.body.id_colegio,req.body.id_nivel_educativo]).catch(err => {
+  grados = await pg.func('public.ft_view_get_grados_instituto',req.body.id_colegio).catch(err => {
     console.log(err)
   })
 
