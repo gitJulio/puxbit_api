f1d81b667c33b1d9dcf04873ff4aa24e5325c55a
Julio Bueso <julio.ezequiel@gmail.com>
2019-06-27 09:50:27 -0600

    Clear

--- controller/clases/clases-controller.js
+++ controller/clases/clases-controller.js
@@ -2,11 +2,10 @@ var insertClase = require('./insertClase')
 var updateClase = require('./updateClase')
 var deleteClase = require('./deleteClase')
 var getClase = require('./getClase')
-var actualizaClase = require('./actualizaClase')
+
 
 
 exports.insertClase = insertClase.insertClase;
 exports.updateClase = updateClase.updateClase;
 exports.deleteClase = deleteClase.deleteClase;
 exports.getClase = getClase.getClase;
-exports.actualizaClase = actualizaClase.actualizaClase;
--- routes/clases-routes.js
+++ routes/clases-routes.js
@@ -7,6 +7,6 @@ router.post('/insertClase/', clases.insertClase);
 router.post('/updateClase/', clases.updateClase);
 router.post('/deleteClase/', clases.deleteClase);
 router.post('/getClase/', clases.getClase);
-router.post('/actualizaClase/', clases.actualizaClase);
+
 
 module.exports = router
