80ef5230137396a4f5901104e92b2bccb9955364
gitjulio <julio.ezequiel@gmail.com>
2019-06-30 17:32:46 -0600

    update profesores

--- controller/profesores/profesores-controller.js
+++ controller/profesores/profesores-controller.js
@@ -1,6 +1,8 @@
 var insertProfesores = require('./insertProfesores')
 var getProfesores = require('./getProfesores')
+var updateProfesores = require('./updateProfesores')
 
 
 exports.insertProfesores = insertProfesores.insertProfesores;
 exports.getProfesores = getProfesores.getProfesores;
+exports.updateProfesores = updateProfesores.updateProfesores;
--- routes/profesores-routes.js
+++ routes/profesores-routes.js
@@ -5,5 +5,6 @@ let profesores = require('../controller/profesores/profesores-controller')
 
 router.post('/insertProfesores/', profesores.insertProfesores);
 router.post('/getProfesores/', profesores.getProfesores);
+router.post('/updateProfesores/', profesores.updateProfesores);
 
 module.exports = router
