d489c41f9ad7af9aae425783d2c5366f3617b8e9
gitjulio <julio.ezequiel@gmail.com>
2019-07-03 22:00:11 -0600

    getClase

--- controller/clases/getClase.js
+++ controller/clases/getClase.js
@@ -7,9 +7,11 @@ exports.getClase = async function(req, res, next) {
 
 
   let gClase;
-  gClase = await pg.func('public.ft_view_clases', req.body.id_colegio).catch(err => {
+  gClase = await pg.func('public.ft_view_clase', [req.body.id_nivel_educativo, req.body.id_grado, req.body.id_seccion]).catch(err => {
     console.log(err)
   })
+
+  console.log(gClase);
 
   if (res.statusCode != 200) {
     return
