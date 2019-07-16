01678b3802944348b8a7120616f2fdb79e2ff77a
gitjulio <julio.ezequiel@gmail.com>
2019-07-15 23:31:11 -0600

    getAlumnos

--- controller/alumnos/alumnos-controller.js
+++ controller/alumnos/alumnos-controller.js
@@ -1,4 +1,6 @@
 var insertAlumno = require('./insertAlumno')
+var getAlumnos = require('./getAlumnos')
 
 
 exports.insertAlumno = insertAlumno.insertAlumno;
+exports.getAlumnos = getAlumnos.getAlumnos;
--- routes/alumnos-routes.js
+++ routes/alumnos-routes.js
@@ -4,5 +4,6 @@ let alumnos = require('../controller/alumnos/alumnos-controller')
 
 
 router.post('/insertAlumno/', alumnos.insertAlumno);
+router.post('/getAlumnos/', alumnos.getAlumnos);
 
 module.exports = router
