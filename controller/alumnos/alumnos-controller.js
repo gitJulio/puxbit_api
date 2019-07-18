00d6202ab8ba35f7ece149c82a71d9897abc2c39
gitjulio <julio.ezequiel@gmail.com>
2019-07-17 21:19:44 -0600

    update alumnos

--- controller/alumnos/alumnos-controller.js
+++ controller/alumnos/alumnos-controller.js
@@ -1,8 +1,10 @@
 var insertAlumno = require('./insertAlumno')
 var getAlumnos = require('./getAlumnos')
 var eliminarAlumno = require('./eliminarAlumno')
+var updateAlumnos = require('./updateAlumnos')
 
 
 exports.insertAlumno = insertAlumno.insertAlumno;
 exports.getAlumnos = getAlumnos.getAlumnos;
 exports.eliminarAlumno = eliminarAlumno.eliminarAlumno;
+exports.updateAlumnos = updateAlumnos.updateAlumnos;
--- routes/alumnos-routes.js
+++ routes/alumnos-routes.js
@@ -6,5 +6,6 @@ let alumnos = require('../controller/alumnos/alumnos-controller')
 router.post('/insertAlumno/', alumnos.insertAlumno);
 router.post('/getAlumnos/', alumnos.getAlumnos);
 router.post('/eliminarAlumno/', alumnos.eliminarAlumno);
+router.post('/updateAlumnos/', alumnos.updateAlumnos);
 
 module.exports = router
