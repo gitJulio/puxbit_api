7c4baacb6172767297170a7581e5e87085f1f552
gitjulio <julio.ezequiel@gmail.com>
2019-08-29 21:01:35 -0600

    colegio

--- controller/usuarios/getLogin.js
+++ controller/usuarios/getLogin.js
@@ -1,37 +1,38 @@
-
 const pg = require('../../configuracion/ps_connection')
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-exports.getLogin=async function(req, res, next){
+exports.getLogin = async function(req, res, next) {
 
-    
-    let body=req.body;
 
-    let usuario=
-    await pg.func('public.ft_view_get_usuario',[body.email,body.password]).catch(err=>{
-        res.status(500).send({
-        error:err,
-        status:500
+    let body = req.body;
+
+    let usuario =
+        await pg.func('public.ft_view_get_usuario', [body.email, body.password]).catch(err => {
+            res.status(500).send({
+                error: err,
+                status: 500
+            })
         })
-    })
+    console.log(usuario);
 
 
-    if (usuario && usuario !='') {
+    if (usuario && usuario != '') {
         let token = jwt.sign({
-        id:usuario[0].id_usuario,
-        nivel:usuario[0].nivel
-        },process.env.SEED,{ expiresIn:process.env.CADUCIDAD_TOKEN});
-    
+            id: usuario[0].id_usuario,
+            nivel: usuario[0].nivel,
+            id_colegio: usuario[0].id_colegio
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
         res.send({
-        ok:true,
-        usuario:usuario[0].correo,
-        token
+            ok: true,
+            usuario: usuario[0].correo,
+            token
         })
-    }else{
+    } else {
         res.status(400).send({
-        error:"usuario o contraseña invalidos"
+            error: "usuario o contraseña invalidos"
         })
     }
-    
+
 
 }
