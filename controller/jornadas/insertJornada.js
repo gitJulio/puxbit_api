b3d0d366dfbcc8b06ce5dd2537fa4e1c3bef5b03
gitjulio <julio.ezequiel@gmail.com>
2019-09-02 23:05:57 -0600

    insert jornada

--- controller/jornadas/insertJornada.js
+++ controller/jornadas/insertJornada.js
@@ -4,9 +4,14 @@ const pg = require('../../configuracion/ps_connection')
 exports.insertJornada = async function(req, res, next) {
 
 
+  data_jornada=[{
+    descripcion:req.body.descripcion,
+    id_colegio:process.env.id_colegio,
+    id_anio_escolar:process.env.id_anio_escolar
+  }]
 
   let insertarJornada;
-  insertarJornada = await pg.func('public.ft_proc_insertar_jornada', JSON.stringify(req.body)).catch(err => {
+  insertarJornada = await pg.func('public.ft_proc_insertar_jornada', JSON.stringify(data_jornada)).catch(err => {
     console.log(err)
   })
 
