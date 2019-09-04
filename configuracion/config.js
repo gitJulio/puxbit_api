468f70299339f0d7c0ef5269d8e815c10fbdb449
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-04 15:02:14 -0600

    Cambio de user db

--- configuracion/config.js
+++ configuracion/config.js
@@ -7,22 +7,28 @@ process.env.CADUCIDAD_TOKEN_MOVIL = 525600 * 60 * 1000
 process.env.SEED = process.env.SEED || 'es-te-es-el-seed-desarrollo';
 
 const config = {
-    app: {
-        host: "localhost",
-        api_key: "NTQyODQyNzM2MGE4NWI0MDFjNmMzYjZhMDE5NzgyY2Q",
-        backup: false
-    },
-    db: {
-        host: 'dbtiendyn.postgres.database.azure.com',
-        port: "5432",
-        database: 'puxbit',
-        user: 'jbueso@dbtiendyn',
-        password: 'Axalon2019..'
-    },
-    data_token: {
-        caducidad_token: 12 * 60 * 1000,
-        seed: 'es-te-es-el-seed-desarrol'
-    }
+  app: {
+    host: "localhost",
+    api_key: "NTQyODQyNzM2MGE4NWI0MDFjNmMzYjZhMDE5NzgyY2Q",
+    backup: false
+  },
+  db: {
+    host: '52.176.97.243',
+    port: "5432",
+    database: 'puxbit',
+    user: 'puxbit',
+    password: 'Axalon2019..'
+  },
+  data_token: {
+    caducidad_token: 12 * 60 * 1000,
+    seed: 'es-te-es-el-seed-desarrol'
+  }
 };
-
+// db: {
+//     host: 'dbtiendyn.postgres.database.azure.com',
+//     port: "5432",
+//     database: 'puxbit',
+//     user: 'jbueso@dbtiendyn',
+//     password: 'Axalon2019..'
+// },
 module.exports = config;
