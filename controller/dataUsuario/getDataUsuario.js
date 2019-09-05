835a91547493e9d991b506d8378cc7a3b15f782d
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-05 12:14:17 -0600

    InsertAlumno

--- controller/dataUsuario/getDataUsuario.js
+++ controller/dataUsuario/getDataUsuario.js
@@ -7,24 +7,40 @@ exports.getDataUsuario = async function(req, res, next) {
 
 
 
-    let dataUsuario;
-    dataUsuario = await pg.func('public.ft_view_data_profesor', process.env.id_usuario).catch(err => {
-        console.log(err);
-        res.send({
-            status: false
-        })
+  let dataUsuario;
+
+  if (process.env.id_nivel == 1) {
+    /*Data administrador*/
+  }
+
+  if (process.env.id_nivel == 2) {
+    dataUsuario = await pg.func('public.ft_view_profesor_clases', process.env.id_usuario).catch(err => {
+      console.log(err);
+      res.send({
+        status: false
+      })
     })
 
     if (res.statusCode != 200) {
-        return
+      return
     }
 
     if (dataUsuario.length == 0) {
-        res.send({
-            status: false
-        })
+      res.send({
+        status: false
+      })
     } else {
-        res.send(dataUsuario[0]["ft_view_data_profesor"])
+      res.send(dataUsuario[0]["ft_view_profesor_clases"])
     }
+  }
+
+  if (process.env.id_nivel == 3) {
+    /*Data padre*/
+  }
+
+  if (process.env.id_nivel == 4) {
+    /*Data alumno*/
+  }
+
 
 }
