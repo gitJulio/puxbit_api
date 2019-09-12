a7003f3396b65ecc51e9db6640874e881f33a68c
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-12 15:58:19 -0600

    Verifica Boton

--- controller/clases/getRepasoAsistencia.js
+++ controller/clases/getRepasoAsistencia.js
@@ -1,15 +1,14 @@
 const pg = require('../../configuracion/ps_connection')
-const readline = require("readline");
+
 
 
 exports.getRepasoAsistencia = async function(req, res, next) {
 
   req.body[0].id_colegio = process.env.id_colegio;
   req.body[0].id_anio_escolar = process.env.id_anio_escolar;
-
-  console.log(JSON.stringify(req.body));
+  console.log(req.body);
   let clase;
-  clase = await pg.func('public.ft_proc_desibled_boton', JSON.stringify(req.body)).catch(err => {
+  clase = await pg.func('public.ft_proc_deshabilitar_botones_asistencia_app_profesores', JSON.stringify(req.body)).catch(err => {
     console.log(err)
   })
 
@@ -21,7 +20,7 @@ exports.getRepasoAsistencia = async function(req, res, next) {
       status: 'false'
     }])
   } else {
-    res.send(clase[0]["ft_proc_desibled_boton"])
+    res.send(clase[0]["ft_proc_deshabilitar_botones_asistencia_app_profesores"])
   }
 
 
