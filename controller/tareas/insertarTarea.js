b0fbae4631b876c4945da737f8ec4ac69f9e4a47
gitjulio <julio.ezequiel@gmail.com>
2019-10-09 19:51:33 -0600

    Insert tarea

--- controller/tareas/insertarTarea.js
+++ controller/tareas/insertarTarea.js
@@ -5,20 +5,21 @@ exports.insertarTarea = async function(req, res, next) {
 
 
 
-  let grados;
-  grados = await pg.func('public.ft_proc_insert_tarea', process.env.id_colegio).catch(err => {
+
+  let tarea;
+  tarea = await pg.func('public.ft_proc_insert_tarea', JSON.stringify(req.body)).catch(err => {
     console.log(err)
   })
 
   if (res.statusCode != 200) {
     return
   }
-  if (grados.length == 0) {
+  if (tarea.length == 0) {
     res.send([{
       status: 'false'
     }])
   } else {
-    res.send(grados)
+    res.send(tarea[0]["ft_proc_insert_tarea"])
   }
 
 }
