84abc91be4f8049819660cb20c188db075804288
Julio Bueso <julio.ezequiel@gmail.com>
2019-10-15 12:25:19 -0600

    rename

--- routes/profesores_app/tareas-router.js
+++ routes/profesores_app/tareas-router.js
@@ -1,14 +1,14 @@
 let express = require('express')
 let router = express.Router()
-let alumnosPresentaronTarea = require('../../controller/profesores_app/tareas/tareas-profesores-controller')
+let alumnosPresentaronTareaController = require('../../controller/profesores_app/tareas/tareas-profesores-controller')
 var mdAunt = require('../../middlewares/autenticacion')
 
 router.use(mdAunt.content_type)
 router.use(mdAunt.api_key)
 router.use(mdAunt.verificaToken)
 
-router.post('/alumnosPresentaronTarea/', alumnosPresentaronTarea.alumnosPresentaronTarea)
-router.post('/insertaTarea/', alumnosPresentaronTarea.insertaTarea)
+router.post('/alumnosPresentaronTarea/', alumnosPresentaronTareaController.alumnosPresentaronTarea)
+router.post('/revisaTarea/', alumnosPresentaronTareaController.insertaTarea)
 
 
 
